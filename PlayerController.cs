b21e1898e94139312c81843b555cd05a48c8a043
Pierre Cochard <pierre.cochard@orkeis.fr>
2018-04-11 14:11:46 +0200

    correction deplacement diagonal

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,25 +6,27 @@ public class PlayerController : MonoBehaviour {
 
     public float walkSpeed = 1.0f;
 
-    private Vector3 mousePosition;
-    public float moveSpeed = 0.1f;
+    //private Vector3 mousePosition;
+    //public float moveSpeed = 0.1f;
 
     void Start () {
 		
 	}
 
 	void Update () {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * walkSpeed;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * walkSpeed;
+        var x = Input.GetAxis("Horizontal") * walkSpeed;
+        var z = Input.GetAxis("Vertical") * walkSpeed;
 
-        transform.Translate(x, 0, z);
+        //transform.Translate(x, 0, z);
+        var move = new Vector3(x, 0f, z);
+        transform.position += Vector3.ClampMagnitude(move, walkSpeed) * Time.deltaTime;
 
-        if (Input.GetMouseButton(1))
+        /*if (Input.GetMouseButton(1))
         {
             mousePosition = Input.mousePosition;
             mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
             transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-        }
+        }*/
 
         //transform.Rotate();
     }
