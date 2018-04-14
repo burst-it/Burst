c1409c72c025bce24fa8add45a114fdcab694c58
Unknown <vincent.chever@laposte.net>
2018-04-14 17:01:03 +0200

    Ajout du script EnemyManager
    Ajout du script EnemyManager

---
+++ EnemyManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public PlayerController playerHealth;       // Reference to the player's heatlh.
+	public GameObject enemy;                // The enemy prefab to be spawned.
+	public float spawnTime = 3f;            // How long between each spawn.
+	public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+
+	void Start ()
+	{
+		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+
+	void Spawn ()
+	{
+		if(playerHealth.g_health <= 0f)
+		{
+			return;
+		}
+
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+
+		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+
+}
