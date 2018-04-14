b488bf8a64d4e19cdfb92d63d5a74af4876c7b63
Unknown <vincent.chever@laposte.net>
2018-04-14 16:59:43 +0200

    Refactoring ShootingController
    Refactoring ShootingController

--- ShootingController.cs
+++ ShootingController.cs
@@ -4,46 +4,46 @@ using UnityEngine;
 
 public class ShootingController : MonoBehaviour {
 
-	public int damagePerShot = 20;                  // The damage inflicted by each bullet.
-	public float timeBetweenBullets = 0.15f;        // The time between each shot.
-	public float range = 1f;                      // The distance the gun can fire.
+	public int g_damagePerShot = 20;                  // The damage inflicted by each bullet.
+	public float g_timeBetweenBullets = 0.15f;        // The time between each shot.
+	public float g_range = 1f;                      // The distance the gun can fire.
 
-	float timer;                                    // A timer to determine when to fire.
-	Ray shootRay;                                   // A ray from the gun end forwards.
-	RaycastHit shootHit;                            // A raycast hit to get information about what was hit.
-	int shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
-	ParticleSystem gunParticles;                    // Reference to the particle system.
-	LineRenderer gunLine;                           // Reference to the line renderer.
-	AudioSource gunAudio;                           // Reference to the audio source.
-	Light gunLight;                                 // Reference to the light component.
-	float effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
+	float g_timer;                                    // A timer to determine when to fire.
+	Ray g_shootRay;                                   // A ray from the gun end forwards.
+	RaycastHit g_shootHit;                            // A raycast hit to get information about what was hit.
+	int g_shootableMask;                              // A layer mask so the raycast only hits things on the shootable layer.
+	ParticleSystem g_gunParticles;                    // Reference to the particle system.
+	LineRenderer g_gunLine;                           // Reference to the line renderer.
+	AudioSource g_gunAudio;                           // Reference to the audio source.
+	Light g_gunLight;                                 // Reference to the light component.
+	float g_effectsDisplayTime = 0.2f;                // The proportion of the timeBetweenBullets that the effects will display for.
 
 	void Start ()
 	{
 		// Create a layer mask for the Shootable layer.
-		shootableMask = LayerMask.GetMask ("Shootable");
+		g_shootableMask = LayerMask.GetMask ("Shootable");
 
 		// Set up the references.
-		gunParticles = GetComponent<ParticleSystem> ();
-		gunLine = GetComponent <LineRenderer> ();
-		gunAudio = GetComponent<AudioSource> ();
-		gunLight = GetComponent<Light> ();
+		g_gunParticles = GetComponent<ParticleSystem> ();
+		g_gunLine = GetComponent <LineRenderer> ();
+		g_gunAudio = GetComponent<AudioSource> ();
+		g_gunLight = GetComponent<Light> ();
 	}
 
 	void Update ()
 	{
 		// Add the time since Update was last called to the timer.
-		timer += Time.deltaTime;
+		g_timer += Time.deltaTime;
 
 		// If the Fire1 button is being press and it's time to fire...
-		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets)
+		if(Input.GetButton ("Fire1") && g_timer >= g_timeBetweenBullets)
 		{
 			// ... shoot the gun.
 			Shoot ();
 		}
 
 		// If the timer has exceeded the proportion of timeBetweenBullets that the effects should be displayed for...
-		if(timer >= timeBetweenBullets * effectsDisplayTime)
+		if(g_timer >= g_timeBetweenBullets * g_effectsDisplayTime)
 		{
 			// ... disable the effects.
 			DisableEffects ();
@@ -59,54 +59,54 @@ public class ShootingController : MonoBehaviour {
 	public void DisableEffects ()
 	{
 		// Disable the line renderer and the light.
-		gunLine.enabled = false;
-		gunLight.enabled = false;
+		g_gunLine.enabled = false;
+		g_gunLight.enabled = false;
 	}
 
 	void Shoot ()
 	{
 		// Reset the timer.
-		timer = 0f;
+		g_timer = 0f;
 
 		// Play the gun shot audioclip.
-		//gunAudio.Play ();
+		g_gunAudio.Play ();
 
 		// Enable the light.
-		gunLight.enabled = true;
+		g_gunLight.enabled = true;
 
 		// Stop the particles from playing if they were, then start the particles.
-		gunParticles.Stop ();
-		gunParticles.Play ();
+		g_gunParticles.Stop ();
+		g_gunParticles.Play ();
 
 		// Enable the line renderer and set it's first position to be the end of the gun.
-		gunLine.enabled = true;
-		gunLine.SetPosition (0, transform.position);
+		g_gunLine.enabled = true;
+		g_gunLine.SetPosition (0, transform.position);
 
 		// Set the shootRay so that it starts at the end of the gun and points forward from the barrel.
-		shootRay.origin = transform.position;
-		shootRay.direction = transform.forward;
+		g_shootRay.origin = transform.position;
+		g_shootRay.direction = transform.forward;
 
 		// Perform the raycast against gameobjects on the shootable layer and if it hits something...
-		if(Physics.Raycast (shootRay, out shootHit, range))
+		if(Physics.Raycast (g_shootRay, out g_shootHit, g_range))
 		{
 			// Try and find an EnemyHealth script on the gameobject hit.
-			EnemyController enemyHealth = shootHit.collider.GetComponent <EnemyController> ();
+			EnemyController enemyHealth = g_shootHit.collider.GetComponent <EnemyController> ();
 
 			// If the EnemyHealth component exist...
 			if(enemyHealth != null)
 			{
 				// ... the enemy should take damage.
-				enemyHealth.getHit (damagePerShot);
+				enemyHealth.getHit (g_damagePerShot);
 			}
 
 			// Set the second position of the line renderer to the point the raycast hit.
-			gunLine.SetPosition (1, shootHit.point);
+			g_gunLine.SetPosition (1, g_shootHit.point);
 		}
 		// If the raycast didn't hit anything on the shootable layer...
 		else
 		{
 			// ... set the second position of the line renderer to the fullest extent of the gun's range.
-			gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+			g_gunLine.SetPosition (1, g_shootRay.origin + g_shootRay.direction * g_range);
 		}
 	}
 }
