e032c7e2aeece1a7339da24fd8763b5e9ff7fb52
Unknown <vincent.chever@laposte.net>
2018-04-15 11:50:56 +0200

    Modification du script EnemyManager
    Ajout du système de vague

--- EnemyManager.cs
+++ EnemyManager.cs
@@ -4,30 +4,57 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour {
 
-	public PlayerController playerHealth;       // Reference to the player's heatlh.
-	public GameObject enemy;                // The enemy prefab to be spawned.
-	public float spawnTime = 3f;            // How long between each spawn.
-	public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+	public PlayerController g_playerHealth;       // Reference to the player's heatlh.
+	public GameObject g_enemy;                // The enemy prefab to be spawned.
+	public float g_timeBetweenSpawn = 1f;            // How long between each spawn.
+	public float g_timeBeforWave = 10f;
+	public int g_spawnCount;
+	public Transform[] g_spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+	private bool g_isStartSpawning;
+
 
 
 	void Start ()
 	{
-		// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
-		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		g_isStartSpawning = false;
 	}
 
 
-	void Spawn ()
+
+	void Update(){
+		if(GameObject.FindGameObjectsWithTag("Enemy").Length <= 0 && !g_isStartSpawning){
+			StartCoroutine (SpawnWaves ());
+			g_isStartSpawning = true;
+		}
+	}
+
+
+
+	/*************************************************************************************************************/
+	/*												FONCTIONS		  											 */
+	/*************************************************************************************************************/
+
+
+	IEnumerator SpawnWaves ()
 	{
-		if(playerHealth.g_health <= 0f)
+		yield return new WaitForSeconds (g_timeBeforWave);
+		if(g_playerHealth.g_health <= 0f)
 		{
-			return;
+			yield return null;
 		}
 
-		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-
-		// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-		Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		for (int i = 0; i < g_spawnCount; i++)
+		{
+			int spawnPointIndex = Random.Range (0, g_spawnPoints.Length);
+			// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+			Instantiate (g_enemy, g_spawnPoints[spawnPointIndex].position, g_spawnPoints[spawnPointIndex].rotation);
+			if (i == (g_spawnCount-1)) {
+				// reset wave
+				g_isStartSpawning = false;
+			}
+			yield return new WaitForSeconds (g_timeBetweenSpawn);
+		}
 	}
 
 }
